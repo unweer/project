2f92f13f4989389bd885e8988769916cc6d05d91
unweer <ionova.anastasiia.l@gmail.com>
2025-05-28 14:17:43 +0300

    выполнила задание 3.4

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+const servicesData = [
+    {
+        icon: "img/ic_scales.svg",
+        alt: "icon scales",
+        text: "Актуальные статьи"
+    },
+    {
+        icon: "img/ic_note.svg",
+        alt: "icon note",
+        text: "Шаблоны исков и обращений"
+    },
+    {
+        icon: "img/ic_question.svg",
+        alt: "icon question",
+        text: "Ответы на самые популярные вопросы"
+    }
+];
+
 function openModal() {
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
@@ -16,6 +34,20 @@ function closeModal() {
   console.log('Закрыто модальное окно')
     document.getElementById("modal").style.display = "none";
 }
+
+const servicesContainer = document.querySelector('.services');
+
+servicesData.forEach(service => {
+    const serviceItem = document.createElement('div');
+    serviceItem.classList.add('services__item');
+
+    serviceItem.innerHTML = `
+        <img class="services__item_img" src="${service.icon}" alt="${service.alt}">
+        <p class="services__item_text">${service.text}</p>
+    `;
+
+    servicesContainer.appendChild(serviceItem);
+});
 
 window.onclick = function(event) {
   console.log('Закрыто модальное окно нажатием в затемнение')
